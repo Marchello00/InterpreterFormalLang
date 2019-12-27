a5f354fe2637d9d5453a71c73b453b1ef362bc04
Marchello00 <nagov-mark@mail.ru>
2019-12-27 16:45:09 +0300

    read from file fix

--- src/main.cpp
+++ src/main.cpp
@@ -10,16 +10,30 @@ std::string interactive_hello() {
            flex_interpreter.ps1;
 }
 
+void err_file(const char *filename) {
+    std::cerr << "Can't open file " << std::string(filename) << "\n";
+}
+
 int main(int argc, char *argv[]) {
     if (argc > 1) {
         if (!set_file(argv[1])) {
-            std::cerr << "Can't open file " << std::string(argv[1]) << "\n";
+            err_file(argv[1]);
             return 0;
         }
     } else {
         std::cout << interactive_hello();
     }
-    Interpreter interpreter;
+    bool input_file_fl = false;
+    std::ifstream fin;
+    if (argc > 2) {
+        input_file_fl =  true;
+        fin.open(argv[2]);
+        if (!fin) {
+            err_file(argv[2]);
+            return 0;
+        }
+    }
+    Interpreter interpreter((input_file_fl ? fin : std::cin));
     while (!flex_interpreter.eof) {
         flex_interpreter.atStart = true;
         int status = yyparse(&interpreter);
